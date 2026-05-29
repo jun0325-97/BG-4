8093e38da9523ce908087cb031be44b0418407e9
윤영준 <jun0325@inseq.co.kr>
2026-05-29 15:01:56 +0900

    feat: complete custom loading screen with refined isometric dice svg

--- src/components/common/LoadingScreen.tsx
+++ src/components/common/LoadingScreen.tsx
@@ -6,12 +6,7 @@ import diceLoading from '../../assets/images/dice-loading.svg';
 export default function LoadingScreen() {
   return (
     <div className="loading-screen">
-      <div className="dice-container">
-        <img src={diceLoading} alt="Loading" className="dice dice-1" />
-        <img src={diceLoading} alt="Loading" className="dice dice-2" />
-        <img src={diceLoading} alt="Loading" className="dice dice-3" />
-      </div>
-      <div className="loading-text">LOADING...</div>
+      <img src={diceLoading} alt="Loading" className="dice-center" />
     </div>
   );
 }
