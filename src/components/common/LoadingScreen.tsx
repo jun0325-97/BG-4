a325c51c4a6ef57ef36965639fedba9a77c46412
윤영준 <jun0325@inseq.co.kr>
2026-05-29 14:47:49 +0900

    style: change loading screen background to white and add new refined dice svg

--- src/components/common/LoadingScreen.tsx
+++ src/components/common/LoadingScreen.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './LoadingScreen.scss';
 
+import diceLoading from '../../assets/images/dice-loading.svg';
+
 export default function LoadingScreen() {
   return (
     <div className="loading-screen">
       <div className="dice-container">
-        <img src="/favicon.svg" alt="Loading" className="dice dice-1" />
-        <img src="/favicon.svg" alt="Loading" className="dice dice-2" />
-        <img src="/favicon.svg" alt="Loading" className="dice dice-3" />
+        <img src={diceLoading} alt="Loading" className="dice dice-1" />
+        <img src={diceLoading} alt="Loading" className="dice dice-2" />
+        <img src={diceLoading} alt="Loading" className="dice dice-3" />
       </div>
       <div className="loading-text">LOADING...</div>
     </div>
